ae48432cc986eb6adbd6f76a6fbfede9d40dc51e
Pranav Bawa <pb389@afsaccess1.njit.edu>
2017-05-07 12:52:38 -0400

    sign in added

--- login.php
+++ login.php
@@ -3,5 +3,8 @@
 <head><title>Login form</title>
 </head>
 <body>
+<h2>sign in</h2>
+<input type="text" id="username" placeholder="Enter Username" />
+<input type="password" id="password" placeholder="Enter password"/>
 </body>
 </html>
