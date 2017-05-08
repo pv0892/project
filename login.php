0826dc870c32d76f176a7d08e6f160385807279f
Pranav Bawa <pb389@afsaccess1.njit.edu>
2017-05-08 08:53:38 -0400

    sign up

--- login.php
+++ login.php
@@ -10,6 +10,12 @@
 <input type="text" id="username" placeholder="Enter Username" />
 <input type="password" id="password" placeholder="Enter password"/>
 <input type="button" id="login-btn" value="login" />
+    <form action="register.php" class="register">
+      <div id="sign up">
+        <button type="submit">Sign up</button>
+      </div>
+    </form>
+
 </form>
 </div>
 </body>
