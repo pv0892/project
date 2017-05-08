7c2f0f8f0a3fa6d483d75a91e827ca769d63cda6
Pranav Bawa <pb389@afsaccess1.njit.edu>
2017-05-08 09:11:15 -0400

    registration added

---
+++ register.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title> Registration </title>
+</head>
+<body>
+<div class="header">
+<h1> Register </h1>
+</div>
+<form method="post" action="register.php">
+<table>
+<tr>
+<td>First_name:</td>
+<td><input type ="text" name="First_name" class="textInput"> </td>
+</tr>
+<tr>
+<td>Last_name:</td>
+<td><input type ="text" name="Last_name" class="textInput"> </td>
+</tr>
+<tr>
+<td>E-mail::</td>
+<td><input type ="email" name="email" class="textInput"> </td>
+</tr>
+<tr>
+<td>Birthday::</td>
+<td><input type ="date" name="Birthday" class="textInput"> </td>
+</tr>
+<tr>
+<td>Phone:</td>
+<td><input type ="text" name="Phone" class="textInput"> </td>
+</tr>
+<tr>
+<td>Gender:</td>
+<td><input type ="radio" name="Male" value="Male"> </td>
+
+</tr>
+<tr>
+<td><input type="submit" name="register_btn" value="Register"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
