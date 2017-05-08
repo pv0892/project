a3fa838142499e25773c1a333cd102f36bf219f6
Pranav Bawa <pb389@afsaccess1.njit.edu>
2017-05-08 09:57:10 -0400

    password correct

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
  $dsn = 'mysql:host=sql2.njit.edu;dbname=pb389';
  $username = 'pb389';
- $password = 'pb389';
+ $password = '0ykh9TyW';
  try{
    $db = new PDO($dsn,$username,$password);
  }catch (PDOException $e){
